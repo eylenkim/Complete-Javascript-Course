6517ef73efcca556b684666431905bcd1c7c02a5
Eylen Kim <47227173+eylenkim@users.noreply.github.com>
2020-02-19 15:52:49 -0800

    Update script.js

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -127,6 +127,209 @@ the outside(age).
 
 /////////Lecture: 1st Class functions: passing functions as arguements
 
+/*
+		var years = [1997, 2003, 1969, 2001, 2020];
+
+		function arrayCalc(arr, func) {
+			var arrayResult = [];
+
+			for (var i=0; i < arr.length; i++) {
+				//adding/pushing the result of the func funcution into arrayResult
+				arrayResult.push(func(arr[i]));
+			}
+			return arrayResult;
+		}
+
+		function calcAge (element) {
+			return 2020- element;
+		}
+*/
+
+///Lecture: 1st Class functions returning functions
+
+//Example: function that returns multiple interview questions for different jobs
+function interviewQuestion(job){
+	if (job === 'designer') {
+		return function(name) {
+			console.log(name + ", can you explain what UX is?");
+		}
+	} else if (job === 'teacher') {
+		return function(name) {
+			console.log(name + ", what subject do you teach?");
+		}
+	} else {
+		return function(name) {
+			console.log(name + 'What do you do?');
+		}
+	}
+}
+
+//asign job function to a teacher variable
+var teacherQuestion = interviewQuestion('teacher');
+//assign name to the teacher job function
+teacherQuestion('John'); // John, what subject do you teach?
+
+//alternatively, you can do:
+interviewQuestion('designer')('Eylen'); // Eylen, can you explain UX?
+
+
+
+/////Lecture: Immediately invoked function expressions (IIFE)
+
+
+/**weird way: if the only purpose is to hide the score variable from the
+outside which means creating a private variable. then we don't need to declare
+a whole function w/ a name and then call it 
+		function game() {
+			var score = Math.random() * 10;
+			console.log(score >= 5);
+		}
+
+		game();
+
+**/
+
+//IIFE - better way
+(function () {
+	var score = Math.random() * 10;
+	console.log(score >= 5);
+})  
+//invoke it: 
+();
+
+/*if you wrote this function ^^ without the outer parenthesis, then the 
+JS parser will think this function has no name and will throw an error. With a 
+parenthesis, you are creating an anonymous function and tricking the parser
+into thinking it's a expresssion, not declaration. In JS, what's inside a 
+parenthesis, cannot be a statement/declaration, it will treat it as an expression. **/
+
+//console.log(score);
+
+//adding good luck to increase probability of higher score
+(function (goodluck) {
+	var score = Math.random() * 10;
+	//the higher the goodluck, the more it decreases 5 as the minimum basis
+	console.log(score >= 5 - goodluck);
+})  
+//invoke it and put 5 as goodluck parameter: 
+(2);
+
+
+/////Lecture: Closures
+/* A closure is the combination of a function bundled together (enclosed) 
+with references to its surrounding state (lexical environment). In other words,
+a closure gives you access to an outer function's scope from an inner function.
+
+Closures are created when a function is created.
+
+To use a closure, define a function inside another function and expose it.
+To expose it, return it or pass it onto another function.
+
+Use cases for closure: Closures are commonly used to give objects data privacy.  
+When you use closures for data privacy, the enclosed variables are only in scope within 
+the containing (outer) function. You can’t get at the data from an outside scope except 
+through the object’s privileged methods.
+*/
+
+//Closure example:
+function retirement(retirementAge) {
+	var a = ' years left until retirement.';
+	//This function uses retirementAge parameter AND 'a' variable outside of function
+	return function(yearOfBirth) {
+		var age = 2020 - yearOfBirth;
+		console.log((retirementAge - age) + a);
+	}
+}
+
+var retirementUS = retirement(66);
+retirementUS(1997);
+//or:
+retirement(66)(1997);
+
+
+//Closure example from previous lecture:
+/* Non-closure example 
+function interviewQuestion(job){
+	if (job === 'designer') {
+		return function(name) {
+			console.log(name + ", can you explain what UX is?");
+		}
+	} else if (job === 'teacher') {
+		return function(name) {
+			console.log(name + ", what subject do you teach?");
+		}
+	} else {
+		return function(name) {
+			console.log(name + 'What do you do?');
+		}
+	}
+}
+*/
+//Closure example:
+function interviewQuestionClosure(job) {
+	return function (name) {
+	if (job === 'designer') {
+			console.log(name + ", can you explain what UX is?");
+	} else if (job === 'teacher') {
+			console.log(name + ", what subject do you teach?");
+	} else {
+			console.log(name + 'What do you do?');
+	  }
+
+	}
+};
+
+interviewQuestionClosure('designer')('Eylen');
+
+
+//////Lecture: Bind, Call, and Apply
+
+//Example:
+var pat = {
+	name: 'Pat',
+	age: 22,
+	job: 'Environmental Scientist',
+	presentation: function (style, timeOfDay) {
+		if (style === 'formal') {
+			console.log('good morning ladies n gents. How is everyone\'s ' + 
+				timeOfDay + ' going? I\'m ' + this.name 
+				+ ', I\'m a ' + this.age + ' year old ' + this.job + '.');
+		} else if (style === 'casual') {
+			console.log('What\'s good my dude? How is everyone\'s ' + timeOfDay 
+				+ ' going? I\'m ' + this.name + ', I\'m a ' + this.age + ' year old ' + this.job + '.');
+		}
+	}
+};
+
+pat.presentation('casual', 'morning');
+
+//another object w/o presentation method
+var memus = {
+	name: 'Memus',
+	age: 5,
+	job: 'cat'
+};
+
+//how to use CALL on mimi object to use presentation method from pat object
+		//the memus parameter is explicitly calling the THIS variable
+pat.presentation.call(memus, 'formal', 'afternoon');
+
+//pat.presentation.apply(memus, ['friendly', 'mid-morning'])
+
+/**Bind method - its similar to call method, but it doesn't immediately call 
+the method but instead generates a copy of the function so you can store it;
+this can be useful for functions with preset arguments **/
+var patFriendly = pat.presentation.bind(pat, 'casual');
+patFriendly('morning');
+patFriendly('night');
+
+var memusFormal = pat.presentation.bind(memus, 'formal');
+memusFormal('afternoon');
+
+
+
+//Example of bind method but from previous callback function example
+
 var years = [1997, 2003, 1969, 2001, 2020];
 
 function arrayCalc(arr, func) {
@@ -142,3 +345,12 @@ function arrayCalc(arr, func) {
 function calcAge (element) {
 	return 2020- element;
 }
+//
+
+function isFullAge(limit, element) {
+	return element >= limit;
+}
+
+var ages = arrayCalc(years, calcAge);
+
+var fullAgeJapan = arrayCalc(ages,isFullAge.bind())
